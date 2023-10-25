3b48cef161bfd98dd412d6fc086f382a58ed6b99
forroartur <148948167+forroartur@users.noreply.github.com>
2023-10-25 10:45:52 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c, max=0;
+    cout<<"a";
+    cin>>a;
+    cout<<"b";
+    cin>>b;
+    cout<<"c";
+    cin>>c;
+    if(a>max) {max=a;}
+    if(b>max) {max=b;}
+    if(c=max) {max=c;}
+    cout<<max;
+    return 0;
+}
